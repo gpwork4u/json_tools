1a38a60467516ca7639626fb44a558b2143404ed
chunping.wang <chunping.wang@ailabs.tw>
2022-02-11 00:54:15 +0800

    Rename package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="json_search",
+    name="json-tools-python",
     version='0.1.0',
     author="gpwork4u",
     author_email="gpwork4u@gmail.com",
